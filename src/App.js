78ac8acba0c77fb822bc99d141236b04226ab197
bored <127768589+arcreatx@users.noreply.github.com>
2024-02-20 19:25:56 +0700

    data is promise pending

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+const api = {
+  key: "09bdec72452a615c86e929ea3c01ecae",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
+
 function App() {
+
+  const [searchInput, setSearchInput] = useState("");
+  const [searchCity, setSearchCity] = useState("");
+  const [weatherInfo, setWeatherInfo] = useState("")
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("")
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      if (!searchCity) return;
+      setLoading(true);
+      // Process
+      try {
+        const url = `${api.base}weather?q=${searchCity}&units=metric&APPID=${api.key}`
+        const response = await fetch(url);
+        const data = response.json();
+        setWeatherInfo(JSON.stringify(data));
+      } catch (error) {
+        setErrorMessage(error.message)
+      }
+      setLoading(false)
+    }
+    fetchWeatherData();
+  }, [searchCity])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSearchCity(searchInput);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Fetch Data with useEffect</h1>
+      <form>
+        <input 
+        type="text" 
+        placeholder="City" 
+        value={searchInput} 
+        onChange={(e) => setSearchInput(e.target.value)}
+        />
+        <button>Search</button>
+      </form>
+    </>
   );
 }
 
